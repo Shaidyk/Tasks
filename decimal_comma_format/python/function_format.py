14adefdd0242270debea3faeedbf8acb81493ee2
Shaidyk <shaidykandrey@gmail.com>
2021-04-01 12:29:37 +0300

    rename

--- decimal_comma_format/python/function_format.py
+++ decimal_comma_format/python/function_format.py
@@ -6,8 +6,3 @@ def format_digits(value):
 value_list = [1000, 200, 1234567]
 for value in value_list:
     print(format_digits(value))
-
-
-import locale
-locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
-print('{:n}'.format(1234567))
